897f263ab09acdca2c82427fad25c21775d30897
surfseatown <rpattie1115@gmail.com>
2014-02-25 15:39:27 -0500

    added a few Xe135 lines

--- src/xenon_135_11half.f
+++ src/xenon_135_11half.f
@@ -8,29 +8,38 @@ c-----------------------------------------------------------------------------C
       COMMON/RSEED/ISEED1,ISEED2
 
       gweight = 80.4
-      eweight = 18.879
+      eweight = 20.74174
 
       yprob = (gweight+eweight)*rand(1.d0)
 
       if(yprob.le.80.4)then
         Kpar  = 2
         E = 526.561e3
-      else if(yprob.gt.gweight.and.yprob.le.15.34+gweight)then
+      else if(yprob.gt.gweight.and.yprob.le.1.72+gweight)then
+        Kpar  = 1
+        E = 24.600e3
+      else if(yprob.gt.1.72+gweight.and.yprob.le.17.06+gweight)then
         Kpar  = 1
         E = 492.000e3
-      else if(yprob.gt.15.34+gweight.and.yprob.le.18.26+gweight)then
+      else if(yprob.gt.17.06+gweight.and.yprob.le.19.98+gweight)then
         Kpar  = 1
         E = 521.108e3
-      else if(yprob.gt.18.26+gweight)then
+      else if(yprob.gt.19.98+gweight.and.yprob.le.20.599+gweight)then
         Kpar  = 1
         E = 525.419e3
+      else if(yprob.gt.20.599+gweight.and.yprob.le.20.7265+gweight)then
+        Kpar  = 1
+        E = 526.353e3
+      else if(yprob.gt.20.7265+gweight)then
+        Kpar  = 1
+        E = 526.545e3
       endif
 
       z     = 220.0*(1.0 - 2.0*rand(1.d0))
 1200  continue
       x     = 6.23189*(1.0 - 2.0*rand(1.d0))
       y     = 6.23189*(1.0 - 2.0*rand(1.d0))
-      if(sqrt(x**2 + y**2) .gt. 6.23189) goto 1200 ! IR for 2011/2012
+      if(sqrt(x**2 + y**2) .ge. 6.23189) goto 1200 ! IR for 2011/2012
       theta = 1.0- 2.0*rand(1.d0)
       psi   = 2*pi*rand(1.d0)
       u     = dsin(dacos(theta))*dcos(psi)
