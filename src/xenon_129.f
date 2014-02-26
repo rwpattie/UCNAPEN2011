da5e4a1219d299f94609b86661c44f56d2caf118
surfseatown <rpattie1115@gmail.com>
2014-02-26 07:10:35 -0500

    added a few Xe129 lines

--- src/xenon_129.f
+++ src/xenon_129.f
@@ -7,27 +7,46 @@ c-----------------------------------------------------------------------------C
       COMMON/track/E,X,Y,Z,U,V,W,WGHT,KPAR,IBODY,MAT,ILB(5)
       COMMON/RSEED/ISEED1,ISEED2
 
-      conveprob = 94.0*rand(1.d0)
+      eweight = 201.745
+      gweight = 4.59
+      zprob = (eweight+gweight)*rand(1.d0)
 
-      if(conveprob.lt.64.0)then
-        E = 162.0e3
-      else if(conveprob.ge.64.0.and.conveprob.lt.88.5)then
+      if(zprob.lt.78.85)then
+        Kpar  = 1
+        E = 5.017e3
+      else if(zprob.ge.78.85.and.zprob.lt.94.85)then
+        Kpar  = 1
+        E = 24.600e3
+      else if(zprob.ge.94.85.and.zprob.lt.105.58)then
+        Kpar  = 1
+        E = 34.125e3
+      else if(zprob.ge.105.58.and.zprob.lt.107.745)then
+        Kpar  = 1
+        E = 38.436e3
+      else if(zprob.ge.107.745.and.zprob.lt.171.745)then
+        Kpar  = 1
+        E = 162.00e3
+      else if(zprob.ge.171.745.and.zprob.lt.196.245)then
+        Kpar  = 1
         E = 191.11e3
-      else if(conveprob.ge.88.5)then
+      else if(zprob.ge.196.245.and.zprob.lt.201.745)then
+        Kpar  = 1
         E = 195.42e3
+      else if(zprob.ge.eweight)then
+        Kpar  = 2
+        E = 196.56e3
       endif
 
       z     = 220.0*(1.0 - 2.0*rand(1.d0))
 1200  continue
       x     = 6.23189*(1.0 - 2.0*rand(1.d0))
       y     = 6.23189*(1.0 - 2.0*rand(1.d0))
-      if(sqrt(x**2 + y**2) .gt. 6.23189) goto 1200 ! IR for 2011/2012
+      if(sqrt(x**2 + y**2) .ge. 6.23189) goto 1200 ! IR for 2011/2012
       theta = 1.0- 2.0*rand(1.d0)
       psi   = 2*pi*rand(1.d0)
       u     = dsin(dacos(theta))*dcos(psi)
       v     = dsin(dacos(theta))*dsin(psi)
       w     = theta
-      Kpar  = 1
 
       return
       end
